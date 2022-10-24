88ca86d641bdd6d71438765c95aa89968341a0fd
Alexsey Soldatenko <56048862+do-konca@users.noreply.github.com>
2022-10-24 16:33:56 +0300

    Add files via upload

---
+++ lissajousgen.py
@@ -0,0 +1,57 @@
+import numpy as np
+import time
+
+
+class lissajous_figure:
+    """
+    Фигуры Лиссажу.
+    Задаётся набором точек с координатами x и y.
+    """
+    def __init__(self, x_array, y_array):
+        self.x_arr=x_array
+        self.y_arr=y_array
+
+
+class LissajousGenerator:
+    """
+    Генерирует фигуры Лиссажу с заданными параметрами
+    """
+    def __init__(self, resolution=20):
+        self.set_resolution(resolution)
+        
+        # Эта задержка эмулирует процедуру инициализации следующей версии генератора.
+        # Задержка будет убрана после обновления.
+        # Пока не трогать.
+        # P.S. В новом генераторе задержка будет только при инициализации.
+        # Фигуры будут генерироваться так же быстро, как и сейчас.
+        time.sleep(1)
+
+    def set_resolution(self, resolution):
+        """
+        resolution определяет количество точек в кривой
+        """
+        self._resolution = resolution
+        
+    def get_random_figure(self):
+        """
+        TODO
+        """
+        freq_x = np.random.randint(1, 10)
+        freq_y = np.random.randint(1, 10)
+        resolution = np.random.randint(20, high=1000)
+        params = {
+                  'freq_x':freq_x,
+                  'freq_y':freq_y,
+                  'resolution':resolution
+                 }
+        return self.generate_figure(freq_x, freq_y, resolution), params
+        
+    def generate_figure(self, freq_x, freq_y, resolution):
+        """
+        Генерирует фигуру (массивы x и y координат точек) с заданными частотами.
+        """
+        self.set_resolution(resolution)
+        t = np.linspace(0, 2 * np.pi, self._resolution)
+        x = np.sin(freq_x * t)
+        y = np.sin(freq_y * t)
+        return lissajous_figure(x, y)
---
+++ main.py
@@ -0,0 +1,172 @@
+import sys
+import os
+import PyQt5.QtWidgets as qt
+from PyQt5.QtWidgets import QMessageBox
+from PyQt5 import uic, QtGui
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAggBase as FigureCanvas
+import matplotlib.pyplot as plt
+import numpy as np
+import json
+from lissajousgen import LissajousGenerator, lissajous_figure
+
+
+# Настройки фигуры по умолчанию
+default_settings = {
+    "freq_x": 2,
+    "freq_y": 3,
+    "color": "midnightblue",
+    "width": 1,
+    "resolution": 20
+}
+
+
+# Цвета для matplotlib
+with open("mpl.json", mode="r", encoding='utf-8') as f:
+    mpl_color_dict = json.load(f)
+
+
+class LissajousWindow(qt.QMainWindow):
+    def __init__(self):
+        super(LissajousWindow, self).__init__()
+
+        # Загружаем интерфейс из файла
+        uic.loadUi("main_window.ui", self)
+
+        # Ставим версию и иконку
+        with open("version.txt", "r") as f:
+            version = f.readline()
+        self.setWindowTitle("Генератор фигур Лиссажу. Версия {}. CC BY-SA 4.0 Ivanov".format(
+            version
+        ))
+        scriptDir = os.path.dirname(os.path.realpath(__file__))
+        self.setWindowIcon(QtGui.QIcon(scriptDir + os.path.sep + "icon.bmp"))
+
+        # Создаём холст matplotlib
+        self._fig = plt.figure(figsize=(4, 3), dpi=72)
+        # Добавляем на холст matplotlib область для построения графиков.
+        # В общем случае таких областей на холсте может быть несколько
+        # Аргументы add_subplot() в данном случае:
+        # ширина сетки, высота сетки, номер графика в сетке
+        self._ax = self._fig.add_subplot(1, 1, 1)
+
+        # Создаём qt-виджет холста для встраивания холста
+        # matplotlib fig в окно Qt.
+        self._fc = FigureCanvas(self._fig)
+        # Связываем созданный холст c окном
+        self._fc.setParent(self)
+        # Настраиваем размер и положение холста
+        self._fc.resize(400, 300)
+        self._fc.move(20, 20)
+
+        # Первичное построение фигуры
+        self.plot_lissajous_figure()
+
+        self.resize(650, 300)
+
+        self.plot_button.clicked.connect(self.plot_button_click_handler)
+        self.save_button.clicked.connect(self.save_button_click_handler)
+        self.test_button.clicked.connect(self.test_button_click_handler)
+
+    def test_button_click_handler(self):
+        """
+        TODO
+        """
+        number_of_tests = 5
+        folder_name = "test_folder"
+        if not os.path.exists(folder_name):
+            os.makedirs(folder_name)
+        
+        try:
+            for i in range(number_of_tests):
+                self._ax.cla()
+                self.generator = LissajousGenerator()
+                figure, params = self.generator.get_random_figure()
+                self._ax.plot(figure.x_arr, figure.y_arr,
+                              color=default_settings["color"],
+                              linewidth=default_settings["width"])
+                plt.axis("off")
+                image_name = f"test_image_{i+1}"
+                file_name = f"test_info_{i+1}.json"
+                plt.savefig(os.path.join(folder_name, image_name))
+                with open(os.path.join(folder_name, file_name), "w") as f:
+                    json.dump(params, f, indent=6)
+        except Exception as e: print(e)
+            
+
+            
+        
+        
+    def plot_button_click_handler(self):
+        """
+        Обработчик нажатия на кнопку применения настроек
+        """
+        # Получаем данные из текстовых полей
+        settings = {}
+
+        settings["freq_x"] = float(self.freq_x_lineedit.text())
+        settings["freq_y"] = float(self.freq_y_lineedit.text())
+        settings['resolution'] = int(self.resolution_lineedit.text())
+        settings["color"] = mpl_color_dict[self.color_combobox.currentText()]
+        settings["width"] = int(self.width_combobox.currentText())
+        
+
+        # Перестраиваем график
+        self.plot_lissajous_figure(settings)
+
+    def plot_lissajous_figure(self, settings=default_settings):
+        """
+        Обновление фигуры
+        """
+        # Удаляем устаревшие данные с графика
+        for line in self._ax.lines:
+            line.remove()
+
+        # Генерируем сигнал для построения
+        self.generator = LissajousGenerator()
+        figure = self.generator.generate_figure(settings["freq_x"],
+                                                settings["freq_y"],
+                                                settings['resolution'])
+
+        # Строим график
+        self._ax.plot(figure.x_arr, figure.y_arr,
+                      color=settings["color"], linewidth=settings["width"])
+
+        plt.axis("off")
+
+        # Нужно, чтобы все элементы не выходили за пределы холста
+        plt.tight_layout()
+
+        # Обновляем холст в окне
+        self._fc.draw()
+        
+    def save_button_click_handler(self):
+        """
+        Обработчик нажатия на кнопку сохранения настроек
+        """
+        file_path, _ = qt.QFileDialog.getSaveFileName(self, "Сохранение изображения", "C:\\",
+                                                            "PNG(*.png);;JPEG(*.jpg *.jpeg);;All Files(*.*) ")
+        
+        if file_path == "":
+            return
+        else:
+            plt.savefig(file_path)
+
+        
+
+
+if __name__ == "__main__":
+    # Инициализируем приложение Qt
+    app = qt.QApplication(sys.argv)
+
+    # Создаём и настраиваем главное окно
+    main_window = LissajousWindow()
+
+    # Показываем окно
+    main_window.show()
+
+    # Запуск приложения
+    # На этой строке выполнение основной программы блокируется
+    # до тех пор, пока пользователь не закроет окно.
+    # Вся дальнейшая работа должна вестись либо в отдельных потоках,
+    # либо в обработчиках событий Qt.
+    sys.exit(app.exec_())
